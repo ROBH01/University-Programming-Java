67ac37c6bf15796b123b4289c2fa322e5140d963
ROBH01 <SamFisher93gt@>
2020-04-12 19:26:02 +0100

    Second Semester Speeding Ticket Assignment (with hack) v3

--- Speeding Ticket Assignment/src/SpeedingTicket.java
+++ Speeding Ticket Assignment/src/SpeedingTicket.java
@@ -1,14 +1,3 @@
-/**
- * @author Robert Giurgiulescu <s4820793@bournemouth.ac.uk>
- * @id number s4820793
- * @version 6
- * 
- * Date: 06/04/2018
- * Class (1/4)
- * 
- * This program ...
- */
-
 
 public class SpeedingTicket {
 	private int warning = 4;
@@ -17,20 +6,8 @@ public class SpeedingTicket {
 	private int fine3 = 20;
 	private SpeedingTicketValidation validation = new SpeedingTicketValidation();
 
-	/**
-	 * Empty constructor which creates a ?????????????????????
-	 */
 	public SpeedingTicket() {  }
 
-	/**
-	 * Method which takes in the <code>speedLimit</code> and <code>vehicleSpeed</code> from the GUI class
-	 * and returns a string containing the appropriate <code>result</code> back to the GUI class. It can       //I wrote and.. it returns.... should I? or only write into the @return??
-	 * also show a dialog containing a validation message.
-	 * 
-	 * @param speedLimit Speed limit in the area
-	 * @param vehicleSpeed Speed of the vehicle
-	 * @return a string containing the appropriate <code>result</code> based on the speed limit and the speed of the car
-	 */
 	public String getResult(int speedLimit, int vehicleSpeed) {
 		String result = "";
 
@@ -60,19 +37,19 @@ public class SpeedingTicket {
 		}
 		else if (vehicleSpeed < speedLimit+fine1)
 		{
-			return vehicleSpeed - speedLimit+" miles over limit, fined Â£50";
+			return vehicleSpeed - speedLimit+" miles over limit, fined £50";
 		}
 		else if (vehicleSpeed < speedLimit+fine2)
 		{
-			return vehicleSpeed - speedLimit+" miles over limit, fined Â£100";
+			return vehicleSpeed - speedLimit+" miles over limit, fined £100";
 		}
 		else if (vehicleSpeed < speedLimit+fine3)
 		{
-			return vehicleSpeed - speedLimit+" miles over limit, fined Â£150 and 3 points";
+			return vehicleSpeed - speedLimit+" miles over limit, fined £150 and 3 points";
 		}
 		else
 		{
-			return vehicleSpeed - speedLimit+" miles over limit, fined Â£1000 and disqualified";
+			return vehicleSpeed - speedLimit+" miles over limit, fined £1000 and disqualified";
 		}
 		return result;
 	}
